b66c74c544f7ce75e7e6af3626ce7c8ee61140b1
Rudra <rudra.phymos@gmail.com>
2026-02-09 22:05:40 +0530

    added custom cursor

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-
+import CustomCursor from '@/components/CustomCursor'
 
 export const metadata = {
 title: 'BeeTeam',
@@ -10,7 +10,7 @@ description: 'Top-tier video production company',
 export default function RootLayout({ children }) {
 return (
 <html lang="en">
-<body>{children}</body>
+<body><CustomCursor />{children}</body>
 </html>
 )
 }
